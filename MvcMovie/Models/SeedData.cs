5c1fc5bf0fc295d13616a868eaac25662672685e
RHunt20 <22162274+RHunt20@users.noreply.github.com>
2019-11-16 12:58:32 -0600

    Add project files.

---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "The R.M.",
+                        ReleaseDate = DateTime.Parse("2003-1-31"),
+                        Genre = "Comedy",
+                        Rating = "PG",
+                        Price = 1.112M,                      
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Other Side of Heaven",
+                        ReleaseDate = DateTime.Parse("2001-12-14"),
+                        Genre = "Drama",
+                        Rating = "PG",
+                        Price = 4.8M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Meet the Mormons",
+                        ReleaseDate = DateTime.Parse("2014-10-10"),
+                        Genre = "Documentary",
+                        Rating = "PG",
+                        Price = 6.047M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Best Two Years",
+                        ReleaseDate = DateTime.Parse("2004-2-20"),
+                        Genre = "Comedy-Drama",
+                        Rating = "PG",
+                        Price = .5M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
